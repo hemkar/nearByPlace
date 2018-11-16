b22fb1e1dc25d1819a60965f5f23774dd4a596a4
hemkar <kumarhemkar@yahoo.com>
2018-11-16 08:45:21 +0530

    JoinPoint {method signature,method param}

--- src/main/java/com/nearby/aop/BeforeAdvice2.java
+++ src/main/java/com/nearby/aop/BeforeAdvice2.java
@@ -1,8 +1,10 @@
 package com.nearby.aop;
 
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +14,17 @@ import org.springframework.stereotype.Component;
 public class BeforeAdvice2 {
 	// before advice which calls point-cut declaration
 	@Before("com.nearby.aop.PointCuts.forServAndController()")
-	public void before() {
+	public void before(JoinPoint joinPoint) { //joinpoint contains meta data about method being called
 		System.out.println("logging aspect 2 for service and controller");
+		
+		//display method signature
+		MethodSignature sign= (MethodSignature) joinPoint.getSignature();
+		System.out.println(sign);
+		
+		//Display method argument.
+		Object[] args=joinPoint.getArgs();
+		for(Object obj: args){
+			System.out.println("Argument using joinpoint : "+obj);
+		}
 	}
 }
